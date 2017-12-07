d7f9693059af4dd1ddfb527653e2d9b00487361a
tomekbielaszewski <tomekbielaszewski@gmail.com>
2017-12-08 00:28:40 +0100

    tree generating

--- Day 7/part1.go
+++ Day 7/part1.go
@@ -6,30 +6,30 @@ import (
 )
 
 func GetBottomNodeName(input *list.List) (string, error) {
-	programs := make(map[string][]string)
-	programsCopy := make(map[string][]string)
+	nodes := make(map[string][]string)
+	nodesCopy := make(map[string][]string)
 	programNames := list.New()
 
 	for e := input.Front(); e != nil; e = e.Next() {
 		row := strings.Replace(e.Value.(string), ",", "", -1)
 		rowElements := strings.Fields(row)
-		programs[rowElements[0]] = rowElements
-		programsCopy[rowElements[0]] = rowElements
+		nodes[rowElements[0]] = rowElements
+		nodesCopy[rowElements[0]] = rowElements
 		programNames.PushFront(rowElements[0])
 	}
 
 	for e := programNames.Front(); e != nil; e = e.Next() {
-		rowElements := programsCopy[e.Value.(string)]
+		rowElements := nodesCopy[e.Value.(string)]
 
 		if len(rowElements) > 2 {
 			for i := 3; i < len(rowElements); i++ {
 				childElem := rowElements[i]
-				delete(programs, childElem)
+				delete(nodes, childElem)
 			}
 		}
 	}
 
-	for key := range programs {
+	for key := range nodes {
 		return key, nil
 	}
 
--- Day 7/part2.go
+++ Day 7/part2.go
@@ -1,7 +1,92 @@
 package Day_7
 
-import "container/list"
+import (
+	"container/list"
+	"strings"
+	"strconv"
+)
 
 func GetCorrectWeight(input *list.List) (int, error) {
+	rows := make(map[string][]string)
+
+	for e := input.Front(); e != nil; e = e.Next() {
+		row := strings.Replace(e.Value.(string), ",", "", -1)
+		row = strings.Replace(row, "(", "", 1)
+		row = strings.Replace(row, ")", "", 1)
+
+		rowElements := strings.Fields(row)
+		rows[rowElements[0]] = rowElements
+	}
+
+	rootName, _ := GetBottomNodeName(input)
+	/*rootNode, nodes := */ buildTree(rows, rootName)
+
 	return 0, nil
+}
+
+func buildTree(rows map[string][]string, rootName string) (*Node, map[string]*Node) {
+	nodes := make(map[string]*Node)
+
+	rootNode := createDetachedNode(rows[rootName])
+	nodes[rootName] = rootNode
+
+	childrenToProcess := list.New()
+	childrenToProcess.PushBackList(getChildrenNodesToProcess(rootName, rows))
+
+	for e := childrenToProcess.Front(); e != nil; e = e.Next() {
+		child := e.Value.(*NodeToProcess)
+
+		childNode := createDetachedNode(child.childRow)
+		attachNode(childNode, nodes[child.parent])
+		nodes[child.childName] = childNode
+
+		if len(getChildrenFromRow(child.childName, rows)) > 0 {
+			childrenToProcess.PushBackList(getChildrenNodesToProcess(child.childName, rows))
+		}
+	}
+
+	return rootNode, nodes
+}
+
+func createDetachedNode(row []string) *Node {
+	weight, _ := strconv.Atoi(row[1])
+	return &Node{name: row[0], weight: weight, children: list.New()}
+}
+
+func attachNode(child *Node, parent *Node) {
+	parent.children.PushFront(child)
+	if child.parent != nil {
+		panic("Node was already attached, something's fucky!")
+	}
+	child.parent = parent
+}
+
+type Node struct {
+	parent   *Node
+	children *list.List
+
+	name   string
+	weight int
+}
+
+type NodeToProcess struct {
+	parent    string
+	childName string
+	childRow  []string
+}
+
+func getChildrenNodesToProcess(parent string, rows map[string][]string) *list.List {
+	row := rows[parent]
+	childrenNames := row[3:]
+	children := list.New()
+
+	for _, childName := range childrenNames {
+		children.PushFront(&NodeToProcess{parent: parent, childRow: rows[childName], childName: childName})
+	}
+
+	return children
+}
+
+func getChildrenFromRow(parent string, rows map[string][]string) []string {
+	return rows[parent][2:]
 }
