9111175f49a48730fbb21d72646cdc426d47918a
tomekbielaszewski <tomekbielaszewski@gmail.com>
2017-12-07 20:37:36 +0100

    Day 7, part 1 solution found. Used collections like list and map, reading lines from file with a scanner (might break when reading long lines)

--- Day 7/part1.go
+++ Day 7/part1.go
@@ -3,31 +3,60 @@ package Day_7
 import (
 	"os"
 	"bufio"
-	"bytes"
+	"container/list"
+	"strings"
 )
 
-func GetBottomNodeName(input string) string {
-	return input
+func GetBottomNodeName(input *list.List) (string, error) {
+	programs := make(map[string][]string)
+	programsCopy := make(map[string][]string)
+	programNames := list.New()
+
+	for e := input.Front(); e != nil; e = e.Next() {
+		row := strings.Replace(e.Value.(string), ",", "", -1)
+		rowElements := strings.Fields(row)
+		programs[rowElements[0]] = rowElements
+		programsCopy[rowElements[0]] = rowElements
+		programNames.PushFront(rowElements[0])
+	}
+
+	for e := programNames.Front(); e != nil; e = e.Next() {
+		rowElements := programsCopy[e.Value.(string)]
+
+		if len(rowElements) > 2 {
+			for i := 3; i < len(rowElements); i++ {
+				childElem := rowElements[i]
+				delete(programs, childElem)
+			}
+		}
+	}
+
+	for key := range programs {
+		return key, nil
+	}
+
+	var err error
+	return "", err
 }
 
-func ReadFromFile(filename string) string {
-	var buffer bytes.Buffer
+func ReadFromFile(filename string) *list.List {
+	stringLines := list.New()
 
-	file, e := os.Open(filename)
-	check(e)
+	file, err := os.Open(filename)
+	check(err)
 
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		buffer.WriteString(scanner.Text())
+		stringLines.PushFront(scanner.Text())
 	}
 
-	return buffer.String()
+	return stringLines
 }
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
-
--- Day 7/part1_test.go
+++ Day 7/part1_test.go
@@ -6,23 +6,12 @@ import (
 )
 
 func TestDay7_Example1(t *testing.T) {
-	input := "pbga (66)\n" +
-	"xhth (57)\n" +
-	"ebii (61)\n" +
-	"havc (66)\n" +
-	"ktlj (57)\n" +
-	"fwft (72) -> ktlj, cntj, xhth\n" +
-	"qoyq (66)\n" +
-	"padx (45) -> pbga, havc, qoyq\n" +
-	"tknk (41) -> ugml, padx, fwft\n" +
-	"jptl (61)\n" +
-	"ugml (68) -> gyxo, ebii, jptl\n" +
-	"gyxo (61)\n" +
-	"cntj (57)"
+	input := ReadFromFile("example.txt")
 
-	result := GetBottomNodeName(input)
+	result, _ := GetBottomNodeName(input)
 
 	expected := "tknk"
+	fmt.Println("got ", result)
 	if result != expected {
 		t.Error("Expected ", expected, " but got ", result)
 	}
@@ -31,7 +20,7 @@ func TestDay7_Example1(t *testing.T) {
 func TestDay7_Solution(t *testing.T) {
 	input := ReadFromFile("input.txt")
 
-	result := GetBottomNodeName(input)
+	result, _ := GetBottomNodeName(input)
 
 	fmt.Printf("Part 1 solution is: %s \n\n", result)
 }
