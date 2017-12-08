4386986938c678814964bdb853934897b67c84d0
tomekbielaszewski <tomekbielaszewski@gmail.com>
2017-12-08 23:31:07 +0100

    refactor to simplify D4P2

--- Day 4/part1.go
+++ Day 4/part1.go
@@ -10,15 +10,21 @@ func CountValidPassphrases(input *list.List) int {
 	for e := input.Front(); e != nil; e = e.Next() {
 		passphrase := e.Value.(string)
 		words := strings.Fields(passphrase)
-		isPresent := make(map[string]bool)
-
-		for _, word := range words {
-			if isPresent[word] {
-				invalid++
-				break
-			}
-			isPresent[word] = true
+		if isInvalid(words) {
+			invalid++
 		}
 	}
 	return input.Len() - invalid
+}
+
+func isInvalid(words []string) bool {
+	isPresent := make(map[string]bool)
+	for _, word := range words {
+		if isPresent[word] {
+			return true
+			break
+		}
+		isPresent[word] = true
+	}
+	return false
 }
