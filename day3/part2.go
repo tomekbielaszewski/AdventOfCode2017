cbe235009c0c86d155aca615c6039be8c19c0fe9
tomekbielaszewski <tomekbielaszewski@gmail.com>
2018-03-21 15:20:28 +0100

    D3P2 removed test printing

--- day3/part2.go
+++ day3/part2.go
@@ -1,7 +1,5 @@
 package Day_3
 
-import "fmt"
-
 func CalculateWierdSums(input int) int {
 	var board [11][11]int
 	board[5][5] = 1
@@ -11,18 +9,17 @@ func CalculateWierdSums(input int) int {
 	dx := 0
 	dy := 1
 
-
 	for i := 0; i < 10*10; i++ {
-		if dy < 0 && board[x - 1][y] == 0 {
+		if dy < 0 && board[x-1][y] == 0 {
 			dx = -1
 			dy = 0
-		} else if dx < 0 && board[x][y + 1] == 0 {
+		} else if dx < 0 && board[x][y+1] == 0 {
 			dx = 0
 			dy = 1
-		} else if dy > 0 && board[x + 1][y] == 0 {
+		} else if dy > 0 && board[x+1][y] == 0 {
 			dx = 1
 			dy = 0
-		} else if dx > 0 && board[x][y - 1] == 0 {
+		} else if dx > 0 && board[x][y-1] == 0 {
 			dx = 0
 			dy = -1
 		}
@@ -32,20 +29,18 @@ func CalculateWierdSums(input int) int {
 
 		board[x][y] =
 			board[x-1][y] +
-			board[x-1][y-1] +
-			board[x]  [y-1] +
-			board[x+1][y-1] +
-			board[x+1][y] +
-			board[x+1][y+1] +
-			board[x]  [y+1] +
-			board[x-1][y+1]
+				board[x-1][y-1] +
+				board[x]  [y-1] +
+				board[x+1][y-1] +
+				board[x+1][y] +
+				board[x+1][y+1] +
+				board[x]  [y+1] +
+				board[x-1][y+1]
 
 		if board[x][y] > input {
 			return board[x][y]
 		}
 	}
-
-	fmt.Printf("%v", board)
 
 	return -1
 }
